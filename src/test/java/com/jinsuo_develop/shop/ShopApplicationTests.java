ab58d7bd7f39e5623847dc82f3f9bc307a99cb50
Jinsuo <cil04110@gmail.com>
2023-01-31 00:38:39 +0900

    init shop project

---
+++ src/test/java/com/jinsuo_develop/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinsuo_develop.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
